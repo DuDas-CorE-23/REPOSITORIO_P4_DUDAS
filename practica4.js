870dec5ba225b44ccd1eda8ef061964efc06c8fa
Nerio Messino <neriomessino14@outlook.es>
2023-02-28 11:08:21 +0100

    funcion max_temp

--- practica4.js
+++ practica4.js
@@ -4,3 +4,19 @@
 function load(citiesFilename){
   JSON.parse(citiesFilename);
 }
+
+
+function max_temp(cities){
+ let temp1;
+ let temp_max;
+ let tamaño_cities=cities.length;
+ for (let i=0;i<tamaño_cities;i++){
+   if(cities[i]>temp1){
+    cities[i]=temp1;
+    temp_max=temp1;
+    console.log(temp_max);
+   }
+   return temp_max;
+ }
+ return;
+}
