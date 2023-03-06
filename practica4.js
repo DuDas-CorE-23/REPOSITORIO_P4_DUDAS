cffcd022efcdc11542281a6a2b6d8d9a00c72220
Nerio Messino <neriomessino14@outlook.es>
2023-03-06 11:29:46 +0100

    cambiamos max_temp

--- practica4.js
+++ practica4.js
@@ -8,16 +8,14 @@ exports.load = async filename => {
 }
 
 exports.max_temp = (cities) =>{
- let temp_max=cities[0].main.temp;
- let tamaño_cities=cities.length;
- for (let i=1;i<tamaño_cities;i++){
-  let temp_aux=city.main.temp; 
-  if(temp_aux>temp_max){
+  let temp_max =cities[0].main.temp;
+  cities.forEach((city) => {
+   let temp_aux=city.main.temp;
+   if(temp_aux > temp_max){
      temp_max=temp_aux;
-     console.log(temp_max);
-   } 
- }
- return temp_max;
+   }
+  });
+   return temp_max;
 };
 
 exports.min_temp = (cities) =>{
